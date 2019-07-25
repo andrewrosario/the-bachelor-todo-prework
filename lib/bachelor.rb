09aeea87b5a36b50abb91142f4d0c41a8be37dff
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-25 15:06:13 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -58,5 +58,6 @@ def get_average_age_for_season(data, season)
   data[season].each_with_index do |contestant, index|
     ages << data[season][index]['age'].to_i
   end
+  puts "#{ages}, sum #{ages.sum}, number #{ages.length}"
   return (ages.sum/ages.length)
 end
